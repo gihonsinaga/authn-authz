67167621bb404b1cb7c9a9f1ea56ca7711255176
gihon sinaga <89863525+gihonsinaga@users.noreply.github.com>
2024-04-14 20:16:44 +0700

    Create Register.jsx

---
+++ src/Register.jsx
@@ -0,0 +1,116 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import "./index.css";
+
+function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "https://shy-cloud-3319.fly.dev/api/v1/auth/register",
+        {
+          email,
+          name,
+          password,
+        }
+      );
+
+      console.log("response :", response);
+      console.log("response register :", response.data);
+      if (response?.status === 201) {
+        alert("Register berhasil, silahkan login");
+        navigate("/");
+      }
+    } catch (error) {
+      console.error("Registration gagal :", error);
+      setError("Data yang anda masukkan kurang benar silahkan coba lagi");
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-xl shadow-slate-400 w-[700px] rounded px-8 pt-6 pb-8 mb-4"
+      >
+        <div className="flex justify-center text-2xl text-slate-700 font-bold mb-10">
+          Register
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="email"
+          >
+            email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="text"
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="email"
+          >
+            Name
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="Name"
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        {error && <p className="text-red-500">{error}</p>}
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Register
+          </button>
+          <button
+            onClick={() => navigate("/")}
+            className="bg-white hover:bg-blue-500 hover:border-white hover:text-white border-black border-2 text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Register;
